042e9aeeb9fe0b5f1f8480545e9e07c59c2c99fb
piponsio <piponsio96@gmail.com>
2020-04-07 02:47:41 +0100

    functional upgrade - scan class beautified

--- include/scan.hpp
+++ include/scan.hpp
@@ -16,19 +16,24 @@ LIDARLite_v3 myLidarLite;
 class Scan{
 	private:
 		int count_file = 0;
+		bool xyz_log = true;
+		bool info_log = false;
+		bool storm32_log = false;
+		int info = 0;
+
 		Storm32_command my_storm32;
-		bool info = false;
-		bool xyz_file = true;
-		bool info_file = false;
+
 		const char* outfile_name;
-		std::ofstream outfile_xyz;
-		std::ofstream outfile_info;
 		std::string path_xyz;
 		std::string path_info;
+		std::ofstream outfile_xyz;
+		std::ofstream outfile_info;
+
+		std::string prePath();
 		void updatePath(int mod = 0);
 
 	public:
-		Scan(const char* dev = "/dev/ttyACM0", speed_t baud = B9600, const char* outfile_name = "test01", bool xyz_file = 1, bool info_file = 0, int info = 0);
+		Scan(const char* dev = "/dev/ttyACM0", speed_t baud = B9600, const char* outfile_name = "test01", bool xyz_log = 1, bool info_log = 0, bool storm32_log = false, int info = 0);
 		void simple(double x_displacement = 0.0, double y_displacement = 0.0, double z_displacement = 0.0);
 };
 #endif
--- src/scan.cpp
+++ src/scan.cpp
@@ -1,53 +1,20 @@
 #include <scan.hpp>
+
 Scan::Scan(const char* dev, speed_t baud, const char* outfile_name, bool xyz_log, bool info_log, bool storm32_log, int info){
 
-	this->my_storm32 = Storm32_command(dev, baud);
-
-	this->outfile_name = outfile_name;
 	this->xyz_log = xyz_log;
 	this->info_log = info_log;
 	this->storm32_log = storm32_log;
 	this->info = info;
 
+	this->my_storm32 = Storm32_command(dev, baud);
+	this->outfile_name = outfile_name;
+
 	if(xyz_log == 1) this->updatePath(0);
 	if(info_log == 1) this->updatePath(1);
 
 }
-std::string Scan::prePath(){
-		std::string temp = "data/";
-		temp += this->outfile_name;
-		temp += "-";
-		if(this->count_file < 10) temp += "00";
-		else if(this->count_file < 100) temp += "0";
- 		temp += std::to_string(this->count_file);
-	return temp;
-}
-void Scan::updatePath(int mode){
 
-	if(mode==0) this->path_xyz = this->prePath()+".xyz";
-
-
-/*
-		this->path_xyz = "data/";
-		this->path_xyz += this->outfile_name;
-		this->path_xyz += "-";
-		if(this->count_file < 10) this->path_xyz += "00";
-		else if(this->count_file < 100) this->path_xyz += "0";
- 		this->path_xyz += std::to_string(this->count_file);
-*/
-	if(mode==1) this->path_info = this->prePath()+".info";
-
-/*
-		this->path_info = "data/";
-		this->path_info += this->outfile_name;
-		this->path_info += "-";
-		if(this->count_file < 10) this->path_info += "00";
-		else if(this->count_file < 100) this->path_info += "0";
- 		this->path_info += std::to_string(this->count_file);
-		this->path_info += ".info";
-*/
-
-}
 void Scan::simple(double x_displacement, double y_displacement, double z_displacement){
 
 	if(this->storm32_log) this->my_storm32.startLog(this->prePath()+".hsc");
@@ -83,20 +50,11 @@ void Scan::simple(double x_displacement, double y_displacement, double z_displac
 
 	pitch = pitch_min;
 	yaw = yaw_min;
-//	std::cout << "antes de setAngle" << std::endl;
 
 	this->my_storm32.setAngle(pitch, 0.0, yaw);
-//	std::cout << "despuess de setAngle" << std::endl;
 
 	while(angles[0] >= pitch_max + pitch_tolerance || pitch_max < pitch){
-//	std::cout << "antes de getAngle" << std::endl;
-
 		this->my_storm32.getAngles();
-//	std::cout << "despues de getAngle" << std::endl;
-
-//		angles[0] = this->my_storm32.angles[0];
-//		angles[1] = this->my_storm32.angles[1];
-//		angles[2] = this->my_storm32.angles[2];
 		angles[0] = this->my_storm32.getAngle(0);
 		angles[1] = this->my_storm32.getAngle(1);
 		angles[2] = this->my_storm32.getAngle(2);
@@ -105,23 +63,18 @@ void Scan::simple(double x_displacement, double y_displacement, double z_displac
 			std::cout << std::dec << angles[0] << "\t";
 			std::cout << std::dec << angles[1] << "\t";
 			std::cout << std::dec << angles[2] << "\t";
-
 			std::cout << std::dec << pitch_flag << "\t";
 			std::cout << std::dec << yaw_flag << "\t";
-
 			std::cout << std::dec << pitch << "\t";
 			std::cout << std::dec << yaw << std::endl;
-
 		}
 
 		if(this->info_log == 1){
 			this->outfile_info << std::dec << angles[0] << "\t";
 			this->outfile_info << std::dec << angles[1] << "\t";
 			this->outfile_info << std::dec << angles[2] << "\t";
-
 			this->outfile_info << std::dec << pitch_flag << "\t";
 			this->outfile_info << std::dec << yaw_flag << "\t";
-
 			this->outfile_info << std::dec << pitch << "\t";
 			this->outfile_info << std::dec << yaw << std::endl;
 		}
@@ -189,4 +142,19 @@ void Scan::simple(double x_displacement, double y_displacement, double z_displac
 	if(this->info_log == 1) this->outfile_info.close();
 	if(this->storm32_log) this->my_storm32.closeLog();
 
+}
+
+void Scan::updatePath(int mode){
+	if(mode==0) this->path_xyz = this->prePath()+".xyz";
+	if(mode==1) this->path_info = this->prePath()+".info";
+}
+
+std::string Scan::prePath(){
+		std::string temp = "data/";
+		temp += this->outfile_name;
+		temp += "-";
+		if(this->count_file < 10) temp += "00";
+		else if(this->count_file < 100) temp += "0";
+ 		temp += std::to_string(this->count_file);
+	return temp;
 }
